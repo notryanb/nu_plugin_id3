f8b9fd963f12299cb54fed3a49861af89602a265
notryanb@gmail.com <notryanb@gmail.com>
2020-01-30 22:29:10 -0500

    made some changes and I can't get it to install...

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use nu::{
-    serve_plugin, 
-    CallInfo, 
-    Plugin, 
-    Primitive, 
-    ReturnSuccess, 
-    ReturnValue, 
-    ShellError, 
-    Signature,
-    Tagged, 
-    Value,
+use nu_errors::ShellError;
+use nu_plugin::{serve_plugin, Plugin};
+use nu_protocol::{
+    CallInfo, Primitive, ReturnSuccess, ReturnValue, Signature, UntaggedValue, Value,
 };
+
+use id3::Tag as Id3Tag;
 
 struct Id3;
 
@@ -18,23 +13,22 @@ impl Id3 {
         Id3
     }
 
-    fn id3(&mut self, value: Tagged<Value>) -> Result<Tagged<Value>, ShellError> {
-        use id3::{Tag, Version};
+    fn id3(&mut self, value: Value) -> Result<Value, ShellError> {
+        match &value.value {
+            UntaggedValue::Primitive(Primitive::String(s)) => {
+                dbg!(s);
+                let tag = Id3Tag::read_from_path(s).expect("Couldn't read file");
 
-        match value.item {
-            Value::Primitive(Primitive::String(s)) => {
-                let mut tag = Tag::read_from_path(s).unwrap();
-
-                Ok(Tagged {
-                    item: Value::int(s.len() as i64),
+                Ok(Value {
+                    value: tag.artist().unwrap_or("failed artist").into(),
                     tag: value.tag,
                 })
-            },
+            }
             _ => Err(ShellError::labeled_error(
-                    "Unrecognized type in stream",
-                    "'len' given non-string by this",
-                    value.tag.span,
-                    )),
+                "Unrecognized type in stream",
+                "'id3' given non-string by this",
+                value.tag.span,
+            )),
         }
     }
 }
@@ -48,13 +42,11 @@ impl Plugin for Id3 {
         Ok(vec![])
     }
 
-    fn filter(&mut self, input: Tagged<Value>) -> Result<Vec<ReturnValue>, ShellError> {
-        // Ok(vec![ReturnSuccess::value(self.id3(input)?)])
-        Ok(vec![])
+    fn filter(&mut self, input: Value) -> Result<Vec<ReturnValue>, ShellError> {
+        Ok(vec![ReturnSuccess::value(self.id3(input)?)])
     }
 }
 
 fn main() {
     serve_plugin(&mut Id3::new());
 }
-
