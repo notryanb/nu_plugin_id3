054b321dbfd4e2a810f3dc32f90a0e1fc986a033
Ryan Blecher <notryanb@gmail.com>
2020-02-04 21:59:41 -0500

    get basic columns going

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use nu_errors::ShellError;
 use nu_plugin::{serve_plugin, Plugin};
 use nu_protocol::{
-    CallInfo, Primitive, ReturnSuccess, ReturnValue, Signature, UntaggedValue, Value,
+    CallInfo, Primitive, ReturnSuccess, ReturnValue, Signature, TaggedDictBuilder, UntaggedValue, Value,
 };
 
 use id3::Tag as Id3Tag;
@@ -13,16 +13,28 @@ impl Id3 {
         Id3
     }
 
-    fn id3(&mut self, value: Value) -> Result<Value, ShellError> {
+    fn idthree(&mut self, value: Value) -> Result<Value, ShellError> {
         match &value.value {
             UntaggedValue::Primitive(Primitive::String(s)) => {
-                dbg!(s);
                 let tag = Id3Tag::read_from_path(s).expect("Couldn't read file");
+                let mut dict = TaggedDictBuilder::with_capacity(&value.tag, 3);
 
-                Ok(Value {
-                    value: tag.artist().unwrap_or("failed artist").into(),
-                    tag: value.tag,
-                })
+                dict.insert_untagged(
+                    "artist",
+                    UntaggedValue::string(tag.artist().unwrap_or("failed artist").to_string())
+                );
+
+                dict.insert_untagged(
+                    "album",
+                    UntaggedValue::string(tag.album().unwrap_or("failed album").to_string())
+                );
+
+                dict.insert_untagged(
+                    "title",
+                    UntaggedValue::string(tag.title().unwrap_or("failed title").to_string())
+                );
+
+                Ok(dict.into_value())
             }
             _ => Err(ShellError::labeled_error(
                 "Unrecognized type in stream",
@@ -35,7 +47,7 @@ impl Id3 {
 
 impl Plugin for Id3 {
     fn config(&mut self) -> Result<Signature, ShellError> {
-        Ok(Signature::build("id3").filter())
+        Ok(Signature::build("idthree").desc("Display Id3 tag information for mp3 files").filter())
     }
 
     fn begin_filter(&mut self, _: CallInfo) -> Result<Vec<ReturnValue>, ShellError> {
@@ -43,7 +55,7 @@ impl Plugin for Id3 {
     }
 
     fn filter(&mut self, input: Value) -> Result<Vec<ReturnValue>, ShellError> {
-        Ok(vec![ReturnSuccess::value(self.id3(input)?)])
+        Ok(vec![ReturnSuccess::value(self.idthree(input)?)])
     }
 }
 
