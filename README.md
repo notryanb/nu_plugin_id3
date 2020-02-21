69ce7afa659a6676d0d36b3b4be3c9276a1a7c4d
Ryan Blecher <notryanb@gmail.com>
2020-02-21 18:44:08 -0500

    Add readme and demo gif

---
+++ README.md
@@ -0,0 +1,40 @@
+# nu_plugin_id3
+
+The `id3` is a plugin for [nushell](https://www.nushell.sh) that helps you read the [id3](https://en.wikipedia.org/wiki/ID3) metadata of your mp3 files.
+This is project is experimental and still under development.
+The public API for the user (flags, command input, etc...) may change while [nushell](https://www.nushell.sh) is still pre `1.0`.
+
+![Example of id3](images/nushell-01.gif "Example of id3")
+
+This project is still lacking many features
+- Ability to parse Id3V1 tags
+- Show tag version information
+- Eliminate non-mp3 files from the output
+- Missing several Id3V2 fields such as comments, data recorded, etc...
+- Ability to write to the tag (mutate artist, album, comments, lyrics, etc...)
+- Ability to convert from older Id3V1 to Id3V2.X
+- A better demo gif :)
+
+## Installation
+
+### Cargo
+`cargo install nu_plugin_id3`
+
+## Build from Source
+- Clone this repository and navigate to the project directory
+- `cargo install --path .`
+- You may need to restart nushell for it to recognize this plugin.
+
+# Usage
+
+Nagivate to a directory that contains files with _id3_ tags.
+The `id3` plugin expects a file path to parse from.
+This is easiest to get by using the `ls` command in [nushell](https://www.nushell.sh).
+
+```
+ls | get name | id3
+```
+
+To take advantage of some fun features of [nushell](https://www.nushell.sh) like viewing images in your terminal,
+you'll need to have the `binaryview` plugin installed with [nushell](https://www.nushell.sh).
+You can install this via `cargo install nu_plugin_binaryview`. 
--- src/parse.rs
+++ src/parse.rs
@@ -6,79 +6,6 @@ use nu_protocol::{
     Primitive, TaggedDictBuilder, UntaggedValue, Value,
 };
 
-
-// pub struct Id3Tag {
-//     version: Id3Version,
-//     pub title: Option<String>,
-//     pub album: Option<String>,
-//     pub artist: Option<String>,
-//     pub year: Option<i32>,
-//     pub track_number: Option<u32>,
-//     pub duration: Option<u64>,
-//     pub genre: Option<String>,
-//     pub disc: Option<u32>,
-//     pub pictures: Vec<Picture>,
-//     date_recorded: Option<Id3TimeStamp>,
-//     date_released: Option<Id3TimeStamp>,
-// }
-
-// pub enum Id3Version {
-//     Id3v1,
-//     Id3v22,
-//     Id3v23,
-//     Id3v24,
-// }
-
-// pub struct Id3TimeStamp {
-//     year: Option<i32>,
-//     month: Option<u8>,
-//     day: Option<u8>,
-//     hour: Option<u8>,
-//     minute: Option<u8>,
-//     second: Option<u8>,
-// }
-
-
-// impl Id3Tag {
-    // pub fn new() -> Id3Tag {
-    //     Id3Tag {
-    //         // version,
-    //         title: None,
-    //         album: None,
-    //         artist: None,
-    //         year: None,
-    //         track_number: None,
-    //         duration: None,
-    //         genre: None,
-    //         disc: None,
-    //         // pictures: Vec::new(),
-    //         // date_recorded: None,
-    //         // date_released: None,
-    //     }
-    // }
-// }
-
-// impl From<id3::Tag> for Id3Tag {  
-//     fn from(source_tag: id3::Tag) -> Self {    
-//         Id3Tag {
-//             // version,
-//             title: source_tag.title().map(|s| s.to_string()),
-//             album: source_tag.album().map(|s| s.to_string()),
-//             artist: source_tag.artist().map(|s| s.to_string()),
-//             year: source_tag.year(),
-//             track_number: source_tag.track(),
-//             duration: source_tag.duration().map(|d| d as u64),
-//             genre: source_tag.genre().map(|s| s.to_string()),
-//             disc: source_tag.disc(),
-//             pictures: source_tag
-//                 .pictures()
-//                 .map::<id3::frame::Picture, Picture>(|p| Picture::from(p.into())).collect::<Vec<Picture>>(),
-//             // date_recorded: source_tag.date_recorded(),
-//             // date_released: source_tag.date_released(),
-//         }
-//     }
-// }
-
 pub fn parse_id3_tag(value: Value) -> Result<Value, ShellError> {
     match &value.value {
         UntaggedValue::Primitive(Primitive::String(s)) => {
@@ -88,77 +15,50 @@ pub fn parse_id3_tag(value: Value) -> Result<Value, ShellError> {
                 Ok(tag) =>  {                            
                     let mut dict = TaggedDictBuilder::new(&value.tag);
                     
-
-                    
-                    // dict.insert_untagged(
-                    //     "date released",
-                    //     UntaggedValue::string(tag.date_released().unwrap_or(id3::Timestamp {
-                    //         year: 0,
-                    //         month: None,
-                    //         day: None,
-                    //         hour: None,
-                    //         minute: None,
-                    //         second: None,
-                    //     }).to_string())
-                    // );
-
-                    // dict.insert_untagged(
-                    //     "date recorded",
-                    //     UntaggedValue::string(tag.date_recorded().unwrap_or(id3::Timestamp {
-                    //         year: 0,
-                    //         month: None,
-                    //         day: None,
-                    //         hour: None,
-                    //         minute: None,
-                    //         second: None,
-                    //     }).to_string())
-                    // );
-
                     dict.insert_untagged(
                         "title",
-                        UntaggedValue::string(tag.title().unwrap_or(""))
+                        tag.title().map_or_else(|| UntaggedValue::nothing(), |title| UntaggedValue::string(title))
                     );
-                    
+
                     dict.insert_untagged(
                         "album",
-                        UntaggedValue::string(tag.album().unwrap_or(""))
+                        tag.album().map_or_else(|| UntaggedValue::nothing(), |album| UntaggedValue::string(album))
                     );
                     
                     dict.insert_untagged(
                         "artist",
-                        UntaggedValue::string(tag.artist().unwrap_or(""))
+                        tag.artist().map_or_else(|| UntaggedValue::nothing(), |artist| UntaggedValue::string(artist))
                     );
 
                     dict.insert_untagged(
                         "year",
-                        UntaggedValue::int(tag.year().unwrap_or(0))
+                        tag.year().map_or_else(|| UntaggedValue::nothing(), |year| UntaggedValue::int(year))
                     );
 
                     dict.insert_untagged(
                         "track number",
-                        UntaggedValue::int(tag.track().unwrap_or(0))
+                        tag.track().map_or_else(|| UntaggedValue::nothing(), |track| UntaggedValue::int(track))
                     );
 
                     dict.insert_untagged(
                         "duration",
-                        UntaggedValue::duration(tag.duration().map(|d| d as u64).unwrap_or(0))
+                        tag.duration().map_or_else(|| UntaggedValue::nothing(), |duration| UntaggedValue::duration(duration as u64))
                     );
 
                     dict.insert_untagged(
                         "genre",
-                        UntaggedValue::string(tag.genre().unwrap_or(""))
+                        tag.genre().map_or_else(|| UntaggedValue::nothing(), |genre| UntaggedValue::string(genre))
                     );
 
                     dict.insert_untagged(
                         "disc",
-                        UntaggedValue::int(tag.disc().unwrap_or(0))
+                        tag.disc().map_or_else(|| UntaggedValue::nothing(), |disc| UntaggedValue::int(disc))
                     );
-
 
                     let pictures = tag.pictures();
                     let mut pictures_dict = TaggedDictBuilder::new(&value.tag);
 
-                    for pic in pictures {
+                    for pic in pictures.into_iter() {
                         pictures_dict.insert_untagged(
                             "mime type",
                             UntaggedValue::string(&pic.mime_type)
@@ -180,10 +80,17 @@ pub fn parse_id3_tag(value: Value) -> Result<Value, ShellError> {
                         );
                     }
 
-                    dict.insert_value(
-                        "pictures",
-                        pictures_dict.into_value()
-                    );
+                    if pictures_dict.is_empty() {
+                        dict.insert_untagged(
+                            "pictures",
+                            UntaggedValue::nothing()
+                        );    
+                    } else {
+                        dict.insert_value(
+                            "pictures",
+                            pictures_dict.into_value()
+                        );
+                    }
 
                     Ok(dict.into_value())
                 },
@@ -221,25 +128,6 @@ pub fn parse_id3_tag(value: Value) -> Result<Value, ShellError> {
         )),
     }
 }
-
-
-// pub struct Picture {
-//     pub mime_type: String,
-//     pub picture_type: String,
-//     pub description: String,
-//     pub data: Vec<u8>,
-// }
-
-// impl From<id3::frame::Picture> for Picture {  
-//     fn from(source_picture: id3::frame::Picture) -> Self {    
-//         Picture {
-//             mime_type: source_picture.mime_type,
-//             picture_type: picture_type_to_string(source_picture.picture_type),
-//             description: source_picture.description,
-//             data: source_picture.data,
-//         }
-//     }
-// }
 
 fn picture_type_to_string(picture_type: id3::frame::PictureType) -> String {
     let pic_type_str = match picture_type {
