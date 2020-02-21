4efeee8219f2bd15595163dc6fe8a3d44e49c22f
Ryan Blecher <notryanb@gmail.com>
2020-02-20 19:49:08 -0500

    trying to figure out a better way to turn the data structure into a Value

--- src/main.rs
+++ src/main.rs
@@ -10,52 +10,46 @@ use id3::Tag as Id3Tag;
 use parse::Id3Tag as MyTag;
 
 
-struct Id3;
-
-fn picture_type_to_string(picture_type: id3::frame::PictureType) -> String {
-    let pic_type_str = match picture_type {
-        id3::frame::PictureType::Other => "other",
-        id3::frame::PictureType::Icon => "icon",
-        id3::frame::PictureType::OtherIcon => "other icon",
-        id3::frame::PictureType::CoverFront => "front cover",
-        id3::frame::PictureType::CoverBack => "back cover",
-        id3::frame::PictureType::Leaflet => "leaflet",
-        id3::frame::PictureType::Media => "media",
-        id3::frame::PictureType::LeadArtist => "lead artist",
-        id3::frame::PictureType::Artist => "artist",
-        id3::frame::PictureType::Conductor => "conductor",
-        id3::frame::PictureType::Band => "band",
-        id3::frame::PictureType::Composer => "composer",
-        id3::frame::PictureType::Lyricist => "lyricist",
-        id3::frame::PictureType::RecordingLocation => "recording location",
-        id3::frame::PictureType::DuringRecording => "during recording",
-        id3::frame::PictureType::DuringPerformance => "during performance",
-        id3::frame::PictureType::ScreenCapture => "screen capture",
-        id3::frame::PictureType::BrightFish => "bright fish",
-        id3::frame::PictureType::Illustration => "illustration",
-        id3::frame::PictureType::BandLogo => "band logo",
-        id3::frame::PictureType::PublisherLogo => "publisher logo",
-        _ => "undefined",
-    };
-
-    pic_type_str.to_string()
+#[derive(Default)]
+struct Id3 {
+    id3s: Vec<Value>
 }
 
 impl Id3 {
     fn new() -> Id3 {
-        Id3
+        Id3 {
+            id3s: Vec::new()
+        }
     }
 
-    fn idthree(&mut self, value: Value) -> Result<Value, ShellError> {
+    fn id3(&mut self, value: Value) -> Result<Value, ShellError> {
+        let nu_tag = value.tag.clone();
+
         match &value.value {
             UntaggedValue::Primitive(Primitive::String(s)) => {
                 let tag = Id3Tag::read_from_path(s);
 
+
                 match tag {
                     Ok(tag) => {
                         let my_tag = MyTag::from(tag);
+                        self.id3s.push(my_tag.into());
 
-                        let mut dict = TaggedDictBuilder::with_capacity(&value.tag, 8);
+                        Ok(Value { value: UntaggedValue::Table(self.id3s), tag: nu_tag })
+
+                        // match my_tag.to_json() {
+                        //     Ok(json) => {
+
+                        //         Ok(json.into())
+                        //     },
+                        //     _ => Err(ShellError::labeled_error(
+                        //         "Unrecognized type in stream",
+                        //         "'id3' given non-string by this",
+                        //         value.tag.span,
+                        //     ))
+                        // }
+
+                        // let mut dict = TaggedDictBuilder::with_capacity(&value.tag, 8);
                         // let pictures = tag.pictures();
 
                         // let mut pictures_dict = TaggedDictBuilder::new(&value.tag);
@@ -87,45 +81,45 @@ impl Id3 {
                         //     pictures_dict.into_value()
                         // );
         
-                        dict.insert_untagged(
-                            "title",
-                            UntaggedValue::string(my_tag.title.unwrap_or(String::new()))
-                        );
+                        // dict.insert_untagged(
+                        //     "title",
+                        //     UntaggedValue::string(my_tag.title.unwrap_or(String::new()))
+                        // );
                         
-                        dict.insert_untagged(
-                            "album",
-                            UntaggedValue::string(my_tag.album.unwrap_or(String::new()))
-                        );
+                        // dict.insert_untagged(
+                        //     "album",
+                        //     UntaggedValue::string(my_tag.album.unwrap_or(String::new()))
+                        // );
                         
-                        dict.insert_untagged(
-                            "artist",
-                            UntaggedValue::string(my_tag.artist.unwrap_or(String::new()))
-                        );
+                        // dict.insert_untagged(
+                        //     "artist",
+                        //     UntaggedValue::string(my_tag.artist.unwrap_or(String::new()))
+                        // );
         
-                        dict.insert_untagged(
-                            "year",
-                            UntaggedValue::int(my_tag.year.unwrap_or(0))
-                        );
+                        // dict.insert_untagged(
+                        //     "year",
+                        //     UntaggedValue::int(my_tag.year.unwrap_or(0))
+                        // );
         
-                        dict.insert_untagged(
-                            "track number",
-                            UntaggedValue::int(my_tag.track_number.unwrap_or(0))
-                        );
+                        // dict.insert_untagged(
+                        //     "track number",
+                        //     UntaggedValue::int(my_tag.track_number.unwrap_or(0))
+                        // );
         
-                        dict.insert_untagged(
-                            "duration",
-                            UntaggedValue::duration(my_tag.duration.unwrap_or(0))
-                        );
+                        // dict.insert_untagged(
+                        //     "duration",
+                        //     UntaggedValue::duration(my_tag.duration.unwrap_or(0))
+                        // );
 
-                        dict.insert_untagged(
-                            "genre",
-                            UntaggedValue::string(my_tag.genre.unwrap_or(String::new()))
-                        );
+                        // dict.insert_untagged(
+                        //     "genre",
+                        //     UntaggedValue::string(my_tag.genre.unwrap_or(String::new()))
+                        // );
         
-                        dict.insert_untagged(
-                            "disc",
-                            UntaggedValue::int(my_tag.disc.unwrap_or(0))
-                        );
+                        // dict.insert_untagged(
+                        //     "disc",
+                        //     UntaggedValue::int(my_tag.disc.unwrap_or(0))
+                        // );
         
                         // dict.insert_untagged(
                         //     "date released",
@@ -151,7 +145,7 @@ impl Id3 {
                         //     }).to_string())
                         // );
         
-                        Ok(dict.into_value())
+                        // Ok(dict.into_value())
 
                     }
                     Err(_err) => {
@@ -192,7 +186,7 @@ impl Id3 {
 
 impl Plugin for Id3 {
     fn config(&mut self) -> Result<Signature, ShellError> {
-        Ok(Signature::build("idthree").desc("Display Id3 tag information for mp3 files").filter())
+        Ok(Signature::build("id3").desc("Display Id3 tag information for mp3 files").filter())
     }
 
     fn begin_filter(&mut self, _: CallInfo) -> Result<Vec<ReturnValue>, ShellError> {
@@ -200,7 +194,7 @@ impl Plugin for Id3 {
     }
 
     fn filter(&mut self, input: Value) -> Result<Vec<ReturnValue>, ShellError> {
-        Ok(vec![ReturnSuccess::value(self.idthree(input)?)])
+        Ok(vec![ReturnSuccess::value(self.id3(input)?)])
     }
 }
 
--- src/parse.rs
+++ src/parse.rs
@@ -1,3 +1,7 @@
+// use serde::{Deserialize, Serialize};
+// use serde_json::Result;
+
+#[derive(Serialize)]
 pub struct Id3Tag {
     // version: Id3Version,
     pub title: Option<String>,
@@ -8,6 +12,7 @@ pub struct Id3Tag {
     pub duration: Option<u64>,
     pub genre: Option<String>,
     pub disc: Option<u32>,
+    // pub pictures: Vec<Picture>,
     // date_recorded: Option<Id3TimeStamp>,
     // date_released: Option<Id3TimeStamp>,
 }
@@ -28,23 +33,25 @@ pub struct Id3Tag {
 //     second: Option<u8>,
 // }
 
-impl Id3Tag {
-    pub fn new() -> Id3Tag {
-        Id3Tag {
-            // version,
-            title: None,
-            album: None,
-            artist: None,
-            year: None,
-            track_number: None,
-            duration: None,
-            genre: None,
-            disc: None,
-            // date_recorded: None,
-            // date_released: None,
-        }
-    }
-}
+
+// impl Id3Tag {
+    // pub fn new() -> Id3Tag {
+    //     Id3Tag {
+    //         // version,
+    //         title: None,
+    //         album: None,
+    //         artist: None,
+    //         year: None,
+    //         track_number: None,
+    //         duration: None,
+    //         genre: None,
+    //         disc: None,
+    //         // pictures: Vec::new(),
+    //         // date_recorded: None,
+    //         // date_released: None,
+    //     }
+    // }
+// }
 
 impl From<id3::Tag> for Id3Tag {  
     fn from(source_tag: id3::Tag) -> Self {    
@@ -58,8 +65,57 @@ impl From<id3::Tag> for Id3Tag {
             duration: source_tag.duration().map(|d| d as u64),
             genre: source_tag.genre().map(|s| s.to_string()),
             disc: source_tag.disc(),
+            // pictures: source_tag.pictures().map::<id3::frame::Picture, Picture>(|p| Picture::from(p.into())).collect::<Vec<Picture>>(),
             // date_recorded: source_tag.date_recorded(),
             // date_released: source_tag.date_released(),
         }
     }
+}
+
+
+pub struct Picture {
+    pub mime_type: String,
+    pub picture_type: String,
+    pub description: String,
+    pub data: Vec<u8>,
+}
+
+impl From<id3::frame::Picture> for Picture {  
+    fn from(source_picture: id3::frame::Picture) -> Self {    
+        Picture {
+            mime_type: source_picture.mime_type,
+            picture_type: picture_type_to_string(source_picture.picture_type),
+            description: source_picture.description,
+            data: source_picture.data,
+        }
+    }
+}
+
+fn picture_type_to_string(picture_type: id3::frame::PictureType) -> String {
+    let pic_type_str = match picture_type {
+        id3::frame::PictureType::Other => "other",
+        id3::frame::PictureType::Icon => "icon",
+        id3::frame::PictureType::OtherIcon => "other icon",
+        id3::frame::PictureType::CoverFront => "front cover",
+        id3::frame::PictureType::CoverBack => "back cover",
+        id3::frame::PictureType::Leaflet => "leaflet",
+        id3::frame::PictureType::Media => "media",
+        id3::frame::PictureType::LeadArtist => "lead artist",
+        id3::frame::PictureType::Artist => "artist",
+        id3::frame::PictureType::Conductor => "conductor",
+        id3::frame::PictureType::Band => "band",
+        id3::frame::PictureType::Composer => "composer",
+        id3::frame::PictureType::Lyricist => "lyricist",
+        id3::frame::PictureType::RecordingLocation => "recording location",
+        id3::frame::PictureType::DuringRecording => "during recording",
+        id3::frame::PictureType::DuringPerformance => "during performance",
+        id3::frame::PictureType::ScreenCapture => "screen capture",
+        id3::frame::PictureType::BrightFish => "bright fish",
+        id3::frame::PictureType::Illustration => "illustration",
+        id3::frame::PictureType::BandLogo => "band logo",
+        id3::frame::PictureType::PublisherLogo => "publisher logo",
+        _ => "undefined",
+    };
+
+    pic_type_str.to_string()
 }
